2f2290428ef2970b67bcab6dfda82745381d03d4
Kate Kumpanska <kate.kumpansky@gmail.com>
2026-03-04 19:25:40 +0200

    feat: add project

---
+++ parLab1/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static int numberThreads = 2;
+    static int[] steps = new int[numberThreads];
+    static int[] duration = new int[numberThreads];
+    static bool[] stopAllowed;
+    static object lockObject = new object();
+
+    static void Main()
+    {
+        GenerateStepsDurations();   
+        stopAllowed = new bool[numberThreads];
+        Thread[] workerThreads = CreateWorkers();
+        Thread controller = CreateController();
+        controller.Start();
+        foreach (Thread t in workerThreads)
+            {
+                t.Start();
+            }
+        controller.Join();
+        foreach (Thread t in workerThreads)
+        {
+            t.Join();
+        }
+    }
+    static void GenerateStepsDurations()
+    {
+        Random random = new Random();
+        for (int i = 0; i < numberThreads; i++)
+        {
+            duration[i] = random.Next(1000, 5000);
+            steps[i] = random.Next(1, 100);
+        }
+    }
+
+    static Thread[] CreateWorkers()
+    {
+        Thread[] threads = new Thread[numberThreads];
+        for (int i = 0; i < numberThreads; i++)
+        {
+            int index = i;
+            threads[i] = new Thread(() => SumElements(index));
+        }
+        return threads;
+    }
+    static void SumElements(int index)
+    {
+        int  step = steps[index];
+        long sum  = 0;
+        long count = 0;
+        long currentElement = 0;
+        while (true)
+        {
+            bool shouldStop;
+            lock (lockObject)
+            {
+                shouldStop = stopAllowed[index];
+            }
+            if (shouldStop) 
+            {
+                break;
+            }
+            sum            += currentElement;
+            currentElement += step;
+            count++;
+        }
+
+        Console.WriteLine($"Thread {index+1}: sum={sum}, count={count}");
+    }
+    static Thread CreateController()
+    {
+        return new Thread(() =>
+        {
+            Thread[] timers = CreateTimers();
+            foreach (Thread t in timers)
+                t.Start();
+            foreach (Thread t in timers)
+                t.Join();
+        });
+    }
+    static Thread[] CreateTimers()
+    {
+        Thread[] timers = new Thread[numberThreads];
+        for (int i = 0; i < numberThreads; i++)
+        {
+            int index    = i;
+            int waitTime = duration[index];
+            timers[i] = new Thread(() =>
+            {
+                Thread.Sleep(waitTime);           
+                lock (lockObject)                    
+                {
+                    stopAllowed[index] = true;
+                }
+            });
+        }
+        return timers;
+    }
+}
